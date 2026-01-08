0db080c27f7c450e6eaf27b8025ace43b0c9caf8
anashali01 <saiyedanashali@gmail.com>
2026-01-08 10:58:21 +0530

    first commit

---
+++ Js_basic/script.js
@@ -0,0 +1,84 @@
+// let arr = new Array(10,20,30,40);
+// console.log(arr);
+
+// let arr2 = [100,200,300,400]
+// console.log(arr2);
+
+
+
+//! Push And Pop Method 
+// let arr = [10,20]
+
+// console.log(arr);
+// console.log(arr.push(30));
+// console.log(arr);
+
+// console.log(arr.pop());
+
+//! Shift And Unshift 
+// let arr = [10,20,40]
+
+// console.log(arr.unshift(15));
+// console.log(arr);
+
+// console.log(arr.shift());
+// console.log(arr);
+
+//! Splice And Slice 
+// let arr = [10,20,30,40,55]
+
+// console.log(arr.splice(1,2,15,20));
+// console.log(arr);
+
+// console.log(arr.slice(-3));
+
+//! For Loop 
+let arr = [11,20,33,40]
+// let str = ''
+// for(let i = 0;i < arr.length ; i++){
+//     str += arr[i] + ', '
+    
+//     // console.log(arr[i]);
+// }
+// console.log(str);
+
+// for(val in arr){
+//     console.log(arr[val]);
+// }
+
+// for(val of arr){
+//     console.log(val);
+// }
+
+//! ForEach Loop 
+// arr.forEach((val,index)=>{
+//     console.log(val,index);
+// })
+
+//! Filter 
+// let arr2 = arr.filter((val,index)=>{
+
+//     if(val % 2 == 0) return val
+// })
+
+// console.log(arr2);
+
+
+//! FindIndex 
+// let index = arr.findIndex((val)=> {
+//    if(val == 20) return val
+// })
+
+// console.log(index);
+
+//! Find 
+// let data = arr.find((val)=>{
+//     if(val % 2 == 0) return val
+// })
+// console.log(data);
+
+//! Map 
+// let data = arr.map((val)=>{
+//     if(val % 2 == 0) return val
+// })
+// console.log(data);
