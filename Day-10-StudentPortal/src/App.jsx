229509c6a613629b543ec634ef3ad5a3dc496ade
anashali01 <saiyedanashali@gmail.com>
2026-02-17 10:43:51 +0530

    student portal start

---
+++ Day-10-StudentPortal/src/App.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import Home from "./components/Home";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import AddStudent from "./pages/AddStudent";
+import ViewStudent from "./pages/ViewStudent";
+
+const App = () => {
+  const [student, setStudent] = useState({});
+  const [list, setList] = useState([]);
+  const navigate = useNavigate();
+
+  
+
+  useEffect(() => {
+    const storedList = JSON.parse(localStorage.getItem("list") || "[]");
+    setList(storedList);
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem("list" , JSON.stringify(list));
+  },[list])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (student.id) {
+      let newList = list.map((std) => {
+        if (std.id == student.id) {
+          navigate("/viewStudent")
+          return student;
+        }
+        return std;
+      });
+
+      setList(newList);
+    } else {
+      const updatedList = [...list, { ...student, id: Date.now() }];
+      setList(updatedList);
+      navigate('/viewStudent')
+    }
+    setStudent({});
+  };
+
+  const handleDelete = (id) => {
+    let newList = list.filter((s) => s.id !== id);
+    setList(newList);
+  };
+
+  const handleEdit = (id) => {
+    const found = list.find((s) => s.id === id);
+    if (found) {
+      setStudent(found);
+      navigate("/addStudent");
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setStudent({ ...student, [name]: value });
+  };
+
+  console.log(list);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home list={list} />} />
+        <Route
+          path="/addStudent"
+          element={
+            <AddStudent
+              student={student}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+            />
+          }
+        />
+        <Route
+          path="/viewStudent"
+          element={
+            <ViewStudent
+              list={list}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ Day-10-StudentPortal/src/pages/AddStudent.jsx
@@ -0,0 +1,218 @@
+import React from "react";
+import Header from "../components/Header";
+
+const AddStudent = ({student , handleChange , handleSubmit}) => {
+  return (
+    <>
+      <div
+        className="page-wrapper"
+        id="main-wrapper"
+        data-layout="vertical"
+        data-navbarbg="skin6"
+        data-sidebartype="full"
+        data-sidebar-position="fixed"
+        data-header-position="fixed"
+      >
+        <Header />
+        {/*  Main wrapper */}
+        <div className="body-wrapper">
+          {/*  Header Start */}
+          <header className="app-header">
+            <nav className="navbar navbar-expand-lg navbar-light">
+              <ul className="navbar-nav">
+                <li className="nav-item d-block d-xl-none">
+                  <a
+                    className="nav-link sidebartoggler nav-icon-hover"
+                    id="headerCollapse"
+                    href="javascript:void(0)"
+                  >
+                    <i className="ti ti-menu-2" />
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link nav-icon-hover"
+                    href="javascript:void(0)"
+                  >
+                    <i className="ti ti-bell-ringing" />
+                    <div className="notification bg-primary rounded-circle" />
+                  </a>
+                </li>
+              </ul>
+              <div
+                className="navbar-collapse justify-content-end px-0"
+                id="navbarNav"
+              >
+                <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+                  <a
+                    href="https://adminmart.com/product/modernize-free-bootstrap-admin-dashboard/"
+                    target="_blank"
+                    className="btn btn-primary"
+                  >
+                    Download Free
+                  </a>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link nav-icon-hover"
+                      href="javascript:void(0)"
+                      id="drop2"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img
+                        src="../assets/images/profile/user-1.jpg"
+                        alt
+                        width={35}
+                        height={35}
+                        className="rounded-circle"
+                      />
+                    </a>
+                    <div
+                      className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
+                      aria-labelledby="drop2"
+                    >
+                      <div className="message-body">
+                        <a
+                          href="javascript:void(0)"
+                          className="d-flex align-items-center gap-2 dropdown-item"
+                        >
+                          <i className="ti ti-user fs-6" />
+                          <p className="mb-0 fs-3">My Profile</p>
+                        </a>
+                        <a
+                          href="javascript:void(0)"
+                          className="d-flex align-items-center gap-2 dropdown-item"
+                        >
+                          <i className="ti ti-mail fs-6" />
+                          <p className="mb-0 fs-3">My Account</p>
+                        </a>
+                        <a
+                          href="javascript:void(0)"
+                          className="d-flex align-items-center gap-2 dropdown-item"
+                        >
+                          <i className="ti ti-list-check fs-6" />
+                          <p className="mb-0 fs-3">My Task</p>
+                        </a>
+                        <a
+                          href="./authentication-login.html"
+                          className="btn btn-outline-primary mx-3 mt-2 d-block"
+                        >
+                          Logout
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </header>
+          {/*  Header End */}
+          <div className="container-fluid">
+            <div className="container-fluid">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title fw-semibold mb-4">{student.id ? 'Edit' : 'Add'} Student Data</h5>
+                  <div className="card">
+                    <div className="card-body">
+                      <form method="post" onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          >
+                            Roll No
+                          </label>
+                          <input
+                            type="number"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            name="rollNo"
+                            value={student.rollNo || ''}
+                            onChange={handleChange}
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputEmail1"
+                            className="form-label"
+                          >
+                            Name
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            name="name"
+                            value={student.name || ''}
+                            onChange={handleChange}
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          >
+                            Email
+                          </label>
+                          <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            name="email"
+                            value={student.email || ''}
+                            onChange={handleChange}
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          >
+                            Phone Number
+                          </label>
+                          <input
+                            type="tel"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            name="phoneNo"
+                            value={student.phoneNo || ''}
+                            onChange={handleChange}
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          >
+                            Course
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            name="course"
+                            value={student.course || ''}
+                            onChange={handleChange}
+                          />
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                          {student.id ? 'Edit' : 'Add'}
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddStudent;
